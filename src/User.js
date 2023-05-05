a8d977d1ebde295d4df719dce1782d61ff1e9eeb
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-05-05 03:41:43 -0300

    dados dinâmicos - mudar nome e imagem de usuario

--- src/User.js
+++ src/User.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function User() {
+    let [img, setImg] = useState("assets/img/catanacomics.svg");
+    let [userName, setUserName] = useState("catanacomics");
+
+    function trocaImg(){
+        const novaImg = prompt("informe o link da nova imagem")
+        if(novaImg){
+            setImg(novaImg);
+        }
+    }
+    function trocaUserName(){
+        const novoNome = prompt("informe o novo nome de usuário")
+        if (novoNome) {
+            setUserName(novoNome);
+        }
+    }
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img onClick={trocaImg} src={img} alt="imagem de perfil" />
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{userName}</strong>
+                    <ion-icon onClick={trocaUserName} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
